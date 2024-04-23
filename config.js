be16487c1ea48b976162a963e15769d06c462c86
Ezbeng <fajarricky1996@gmail.com>
2024-04-23 09:35:48 +0700

    add konfig vercel.json benarkan database

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection ({
     host: 'sql6.freesqldatabase.com',
     user: 'sql6701162',
     password: 'DjMT2nqwpy',
-    database: 'db_kelontong'
+    database: 'sql6701162'
 })
 
 module.exports = db
